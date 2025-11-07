690141bfb5a8d56630070383a63c2b4b201959ef
Mark Santiago <masantiago01@gmail.com>
2025-11-06 22:34:54 -0800

    fixed some errors in my css and java script

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
 const editProfile = document.querySelector(".profile__edit-profile");
-const edit__Profile__modal = document.querySelector("#edit-profile-modal");
-const modalClose__profile = edit__Profile__modal.querySelector(
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const modalCloseProfile = editProfileModal.querySelector(
   ".modal__close-button"
 );
-const editProfileForm = edit__Profile__modal.querySelector(".modal__form");
-const currentName = edit__Profile__modal.querySelector("#profile-name-input");
-const currentDescription = edit__Profile__modal.querySelector(
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const currentName = editProfileModal.querySelector("#profile-name-input");
+const currentDescription = editProfileModal.querySelector(
   "#profile-description-input"
 );
 
 const newPost = document.querySelector(".profile__new-post");
-const newPost__modal = document.querySelector("#new-post-modal");
-const modalClose__newPost = newPost__modal.querySelector(
-  ".modal__close-button"
-);
-const newPostForm = newPost__modal.querySelector(".modal__form");
-const currentImage = newPost__modal.querySelector("#card-image-input");
-const currentCaption = newPost__modal.querySelector("#card-caption-input");
+const newPostModal = document.querySelector("#new-post-modal");
+const modalCloseNewPost = newPostModal.querySelector(".modal__close-button");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const currentImage = newPostModal.querySelector("#card-image-input");
+const currentCaption = newPostModal.querySelector("#card-caption-input");
 
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
@@ -33,11 +31,11 @@ editProfile.addEventListener("click", function () {
   currentName.value = profileName.textContent;
   currentDescription.value = profileDescription.textContent;
 
-  openModal(edit__Profile__modal);
+  openModal(editProfileModal);
 });
 
-modalClose__profile.addEventListener("click", function () {
-  edit__Profile__modal.classList.remove("modal_is-open");
+modalCloseProfile.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-open");
 });
 
 function handleEditProfileSubmit(evt) {
@@ -46,17 +44,17 @@ function handleEditProfileSubmit(evt) {
   profileName.textContent = currentName.value;
   profileDescription.textContent = currentDescription.value;
 
-  closeModal(edit__Profile__modal);
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 newPost.addEventListener("click", function () {
-  openModal(newPost__modal);
+  openModal(newPostModal);
 });
 
-modalClose__newPost.addEventListener("click", function () {
-  closeModal(newPost__modal);
+modalCloseNewPost.addEventListener("click", function () {
+  closeModal(newPostModal);
 });
 
 function handleAddCardSubmit(evt) {
@@ -65,7 +63,7 @@ function handleAddCardSubmit(evt) {
   console.log(currentImage.value);
   console.log(currentCaption.value);
 
-  closeModal(newPost__modal);
+  closeModal(newPostModal);
 }
 
 newPostForm.addEventListener("submit", handleAddCardSubmit);
